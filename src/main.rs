0ce0a20fd1fff12912daf418d9e25545c39b0daf
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2024-02-01 18:11:57 -0700

    responding hard coded 200

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(stream: &mut TcpStream) -> Result<(), anyhow::Error> {
+    let mut query_bytes = [0u8; 128];
+    stream.read(&mut query_bytes)?;
+    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -6,10 +16,13 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
+    for mut stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(ref mut stream) => {
                 println!("accepted new connection");
+                if let Err(err) = handle_connection(stream) {
+                    println!("connection had error: {}", err)
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
