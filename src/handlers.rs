0658331ec2c9df7da9ba2acfec75f8673dcfbf10
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2024-02-03 13:49:55 -0700

    use read to end instead

--- src/handlers.rs
+++ src/handlers.rs
@@ -9,10 +9,10 @@ use tokio::{io::AsyncReadExt, net::TcpStream};
 use crate::{http_request::HttpRequest, http_serde::HttpDeserialize};
 
 pub async fn handle_connection(mut stream: TcpStream, dir: String) -> anyhow::Result<()> {
-    let mut request_bytes = [0u8; 1000];
-    stream.read(&mut request_bytes).await?;
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer).await?;
 
-    let request = std::str::from_utf8(&request_bytes)?;
+    let request = std::str::from_utf8(&buffer)?;
     let request = HttpRequest::http_deserialize(request)?;
     if request.path == "/" {
         handle_root::handle_root(stream).await?;
